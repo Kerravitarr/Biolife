fc3a5842e7ce2ad9c91c83aba7bb9ab983179642
Ilia Pushkin <rjhjkm20@mail.ru>
2022-02-23 16:27:54 +0300

    Update EvolutionTree.java
    Дерево эволюции снова в строю!
    Передал его полностью, теперь по нему не восстановить эволюцию клеток, однако можно узнать кто кому родственник и на сколько далёкий.
    От корня теперь идут узлы, а ветви срезаются до узлов. Так что сложность такого дерева не больше чем 2^число клеток. Это пожирание памяти считаю удоволетвортильным, главное - больше ни какого бесконечного роста! А эффект есть

--- Java/src/main/EvolutionTree.java
+++ Java/src/main/EvolutionTree.java
@@ -6,59 +6,34 @@ import java.util.Vector;
 import jdk.jfr.Unsigned;
 import panels.JSONmake;
 
-@Deprecated
+//@Deprecated
 public class EvolutionTree {
 
 	static public class Node{
 		/**Время, когда узел появился*/
 		long time = 0;
-		/**Мутации гена*/
-		int newadr = -1;
-		/**Мутации гена*/
-		int newCmd = -1;
-		//Новая фотосинтетичность
-		double nowPh = -1;
-		//Новый фенотип
-		Color color = null;
 		//Поколение в этой ветке
 		long generation = 0;
-		/**Ветвь нашего узла*/
-		String branch = "0";
 		//Потомки нашего узла
 		Vector<Node> child = new Vector<>();
 		/**Счётчик ветвей*/
 		Integer branshCount = 0;
 		/**Число живых потомков*/
-		int countAliveCell = 1;
+		Integer countAliveCell = 0;
 		/**Наш родитель, общий предок, если хотите*/
 		Node perrent = null;
 		
-		public Node newNode(long time,int newadr,int newCmd) {
-			Node node = forAllNode(time);
-			node.newadr = newadr;
-			node.newCmd = newCmd;
-			return node;
-		}
-		public Node newNode(long time,double nowPh) {
-			Node node = forAllNode(time);
-			node.nowPh = nowPh;
-			return node;
-		}
-		public Node newNode(long time,Color color) {
-			Node node = forAllNode(time);
-			node.color = color;
-			return node;
-		}
-		private Node forAllNode(long time) {
+		public Node newNode(long time) {
+			remove(); //Мы больше не служим нашему родителю!
 			Node node = new Node();
-			synchronized (branshCount) {
+			synchronized (this) {
 				node.generation = branshCount + 1;
 				branshCount++;
+				node.countAliveCell = 1;
+				child.add(node);
+				node.perrent = this;
+				node.time = time;
 			}
-			child.add(node);
-			node.perrent = this;
-			node.time = time;
-			node.branch = branch + "->" + node.generation;
 			return node;
 		}
 		/**Создаёт новую ветку, куда будем эволюционировать*/
@@ -69,39 +44,45 @@ public class EvolutionTree {
 		/**Увы, очередной наш потомок того. Если потомков больше нет, то ветвь тупиковая - удаляем!*/
 		public void remove() {
 			countAliveCell--;
-			if(countAliveCell == 0 && child.size() == 0)
-				perrent.remove(this);
+			if(countAliveCell == 0 && child.size() == 0)// У нас нет детей, всё, удаляем у родителя
+				perrent.remove(this); 
+			else if(countAliveCell == 0 && child.size() == 1) 
+				merge();
+			
 		}
 		/**Наш ребёнок сказал нам, что умер. Если это был последний наш ребёнок, то всё - сворачиваем лавочку*/
 		private void remove(Node node) {
 			child.remove(node);
 			if(countAliveCell == 0 && child.size() == 0)
 				perrent.remove(this);
+			else if(countAliveCell == 0 && child.size() == 1)
+				merge();
+		}
+		
+		private void merge() {
+			if(perrent != null) {
+				synchronized (root) {
+					perrent.child.add(child.get(0)); 
+					child.get(0).perrent = perrent;
+					child.get(0).generation = generation;
+				}
+				perrent.remove(this); 
+			} else {
+				root = this;
+			}
 		}
 		
 		public String toString() {
-			String ret = "Поколение " + generation + " мутация произошла в " + time + " от рожденства Адама. Мутация: ";
-			if(newadr != -1)
-				ret += "гена " + newadr + " на " + newCmd;
-			else if(nowPh != -1)
-				ret += " фотосинтетичности, до " + nowPh;
-			else if(color != null)
-				ret += " цвета, до " + color.toString();
-			ret += ". Мутация порадила " + child.size() + " потомков";
+			String ret = "Поколение " + generation + " мутация произошла в " + time + " от рожденства Адама. ";
+			ret += "Мутация порадила " + child.size() + " потомков";
 			return ret;
 		}
 		
 		public JSONmake toJSON() {
 			JSONmake make = new JSONmake();
 			make.add("time", time);
-			make.add("branch", branch);
-			if(newadr != -1) {
-				make.add("newadr", newadr);
-				make.add("newCmd", newCmd);
-			}else if(nowPh != -1)
-				make.add("nowPh", nowPh);
-			else if(color != null)
-				make.add("color", Integer.toHexString(color.getRGB()));
+			make.add("branch", getBranch());
+			make.add("countAliveCell", countAliveCell);
 			
 			JSONmake[] nodes = new JSONmake[child.size()];
 			for (int i = 0; i < nodes.length; i++) {
@@ -110,10 +91,16 @@ public class EvolutionTree {
 			make.add("Nodes", nodes);
 			return make;
 		}
+		public String getBranch() {
+			if(perrent != null)
+				return perrent.getBranch() + "->" + generation;
+			else
+				return "0";
+		}
 	}
 	
 	/**Корень эволюционного дерева, адам*/
-	Node root = new Node();
+	static Node root = new Node();
 	
 	public String toString() {
 		return root.toString();
@@ -124,5 +111,4 @@ public class EvolutionTree {
 		make.add("Node", root.toJSON());
 		return make;
 	}
-	
 }
