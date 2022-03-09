f9607deab0e7f014889c264b31f852f2e9360442
Ilia Pushkin <rjhjkm20@mail.ru>
2022-03-09 08:16:22 +0300

    Create ColorRec.java
    Некий класс, который описывает цветастый прямоугольник на экране.
    Нужна эта вещь, чтобы таких нагененрировать при изменнеии разрешения экрана и потом только обращаться к объектам

---
+++ Java/src/Utils/ColorRec.java
@@ -0,0 +1,34 @@
+package Utils;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class ColorRec{
+	public ColorRec(int x0, int y0, int w, int h,Color c) {
+		x=x0;
+		y=y0;
+		width=w;
+		height=h;
+		setColor(c);
+	}
+	Color color;
+	int x;
+	int y;
+	int width;
+	int height;
+	public void paint(Graphics g) {
+		g.setColor(color);
+		g.fillRect(x,y,width,height);
+	}
+	
+	public String toString() {
+		return "x0: " + x + " y0: " + y + " w: " + width + " h: " + height + " c: " + color;
+	}
+
+	/**
+	 * @param color the color to set
+	 */
+	public void setColor(Color color) {
+		this.color = color;
+	}
+}
