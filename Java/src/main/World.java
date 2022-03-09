b3af932993828cc2a34ba1d3e34f3928224a1c4c
Ilia Pushkin <rjhjkm20@mail.ru>
2022-03-09 08:25:51 +0300

    Update World.java
    Серьёздно оптимизировал код для многопоточности - это позолило добиться ещё большей производительности. Дальше планирую больше. Хочу создать функции создания и регерации карты, но пока не очень придумал как

--- Java/src/main/World.java
+++ Java/src/main/World.java
@@ -6,28 +6,31 @@ import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Vector;
 
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
+import MapObjects.AliveCell;
+import MapObjects.CellObject;
+import MapObjects.Organic;
+import MapObjects.Sun;
+import MapObjects.CellObject.LV_STATUS;
+import MapObjects.CellObject.OBJECT;
+import MapObjects.Geyser;
 import Utils.FPScounter;
 import Utils.JSONmake;
 import Utils.Utils;
-import main.Cell.LV_STATUS;
-import main.Cell.OBJECT;
 import main.Point.DIRECTION;
 import panels.BotInfo;
 import panels.Settings;
 
-public class World extends JPanel {
-
+public class World extends JPanel {	
 	/**Количиство ячеек карты*/
 	public static final Dimension MAP_CELLS = new Dimension(500,200);
 	/**Освещённость карты*/
-	public static double SUN_POWER = 10;
+	public static int SUN_POWER = 10;
 	/**Уровень загрязнения воды*/
 	public static double DIRTY_WATER = 17;
 	/**Степень мутагенности воды*/
@@ -36,8 +39,6 @@ public class World extends JPanel {
 	public static double LEVEL_MINERAL = 0.50;
 	/**Концентрация минералов*/
 	public static double CONCENTRATION_MINERAL = 1;
-	//Сколько тиков подряд обновлять экран
-	public static int FPS_TIC = 1;
 	//Как долго разлагается органика
 	public static int TIK_TO_EXIT = 2;
 	/**Симуляция запущена?*/
@@ -47,28 +48,40 @@ public class World extends JPanel {
 	
 	/**Глобальный мир!*/
 	public static World world = null;
+	/**Солнце нашего мира*/
+	public static Sun sun;
+	/**Гейзер, некая область где вода поднимается снизу вверх*/
+	public static Geyser[] geysers;
+	/**Эволюция ботов нашего мира*/
+	static EvolutionTree tree = new EvolutionTree();
 	
 	class AutostartThread extends Thread{AutostartThread(Runnable target){super(target);start();}}
 	
 	class WorldTask implements Runnable{
-		private Graphics g;
-		Point [] cellsThread;
-		WorldTask(Point [] cellsThread){
-			this.cellsThread= cellsThread;
+		Point [] cellsFirst;
+		Point [] cellsSecond;
+		Point [] activCell = null;
+		WorldTask(Point [] cellsFirst, Point[] cellsSecond){
+			this.cellsFirst= cellsFirst;
+			this.cellsSecond= cellsSecond;
 		}
-		public WorldTask start(Graphics g) {
-			this.g=g;
+		public WorldTask start(boolean isFirst) {
+			activCell = isFirst ? cellsFirst : cellsSecond;
 			return this;
 		}
 		public void run() {
 			/**Перетосовываем, чтобы у всех были равные шансы на ход*/
-			shuffle(cellsThread);
-			for (Point point : cellsThread) {
-				Cell cell= get(point);
-				if(cell != null && cell.stepCount != step) {
-					cell.step(step);
-					if(g != null)
-						cell.paint(g);
+			shuffle(activCell);
+			for (Point point : activCell) {
+				CellObject cell= get(point);
+				if(cell != null && cell.canStep(step)) {
+					try {
+						cell.step(step);
+					} catch (Exception e) {
+						e.printStackTrace();
+						isActiv = false;
+						System.out.println(cell);
+					}
 				}
 			}
 		}
@@ -76,21 +89,21 @@ public class World extends JPanel {
 	
 	/**Шаги мира*/
 	public long step = 0;
+	/**Специальный таймер, который позволяет замедлить симуляцию*/
+	public int timeoutStep = 0;
 	/**Всего точек по процессорам - сколько процессоров, в каждом ряду есть у*/
-	WorldTask [] cells;
+	WorldTask [] cellsTask;
 	Thread [] threads;
-	/**А дополительные клетки - от каждого процессора по 2 ряда, ну и дополнительные*/
-	WorldTask betweenCells;
 	/**Масштаб*/
-	static double scale = 1;
+	public static double scale = 1;
 	/**Высота "неба"*/
 	double UP_border = 0.01;
 	/**Высота "земли"*/
 	double DOWN_border = 0.01;
 	/**Дополнительный край из-за не совершенства арены*/
-	static Dimension border = new Dimension();
+	public static Dimension border = new Dimension();
 	/**Сам мир*/
-	Cell [][] worldMap = new Cell[MAP_CELLS.width][MAP_CELLS.height];
+	CellObject [][] worldMap = new CellObject[MAP_CELLS.width][MAP_CELLS.height];
 	/**Счётчик ФПС*/
 	public final FPScounter fps = new FPScounter();
 	/**Счётчик шагов*/
@@ -105,10 +118,6 @@ public class World extends JPanel {
 	BotInfo info = null;
 	/**Настройки мира*/
 	Settings settings = null;
-	/**Эволюция ботов нашего мира*/
-	EvolutionTree tree = new EvolutionTree();
-	/**Все мегаклетки, чтобы они могли походить после всех*/
-	List<MegaCell> megaCells = new ArrayList<>();
 	/**
 	 * Create the panel.
 	 */
@@ -118,61 +127,28 @@ public class World extends JPanel {
 		info = botInfo;
 		this.settings=settings;
 		
-		int countProc = Runtime.getRuntime().availableProcessors()*2/3;
-		
-		while(MAP_CELLS.width - 2*countProc < countProc)
-			countProc /= 2;
-		
-		/**Сколько рядов уместится в одном процессоре*/
-		int lenghtX = MAP_CELLS.width / countProc;
-		/**Сколько рядов придётся ещё добавить*/
-		int addX = MAP_CELLS.width - lenghtX*countProc;
-		
-		Point [][] cellsThread = new Point[countProc][(lenghtX-2) * MAP_CELLS.height];
-		Point [] cellsMain = new Point[(countProc*2 + addX) * MAP_CELLS.height];
-		
-		int main = 0;
-		for (int proc = 0; proc < countProc; proc++) {
-			for (int x = 0; x < lenghtX; x++) {
-				for (int y = 0; y < MAP_CELLS.height; y++) {
-					if(x >= lenghtX -2 ) {
-						cellsMain[main++] = new Point(x + proc * lenghtX,y);
-					} else {
-						cellsThread[proc][x*MAP_CELLS.height+y] = new Point(x + proc * lenghtX,y);
-					}
-				}
-			}
-		}
-		
-		for (int i = 0; i < addX; i++) {
-			for (int y = 0; y < MAP_CELLS.height; y++) {
-				cellsMain[main++] = new Point(countProc * lenghtX + i,y);
-			}
-		}
-		
-		cells = new WorldTask[cellsThread.length];
-		for (int i = 0; i < cells.length; i++) {
-			cells[i] = new WorldTask(cellsThread[i]);
-		}
-		threads = new Thread[cells.length];
-		betweenCells = new WorldTask(cellsMain);
+		worldGenerate();
 		
 		//Начальные клетки
-		Cell adam = new Cell();
-		adam.pos = new Point(MAP_CELLS.width/2,0);
+		AliveCell adam = new AliveCell();
+		adam.setPos(new Point(MAP_CELLS.width/2,0));
 		EvolutionTree.root.countAliveCell = 1;
 		adam.evolutionNode = EvolutionTree.root;
 		add(adam);
-		
-		recalculate();
-
-		this.settings.updateScrols();
 		
 		setBackground(new Color(255, 255, 255, 255));
 
 		addComponentListener(new ComponentAdapter() {
 			public void componentResized(ComponentEvent e) {
-				newSize(getWidth(),getHeight());
+				double hDel = 1.0 * getHeight() * (1 - (UP_border + DOWN_border)) / (MAP_CELLS.height);
+				double wDel = 1.0 * getWidth() / (MAP_CELLS.width);
+				scale = Math.min(hDel, wDel);
+				border.width = (int) Math.round((getWidth() -MAP_CELLS.width*scale)/2);
+				border.height = (int) Math.round((getHeight() - MAP_CELLS.height*scale)/2);
+				for(Geyser gz : geysers)
+					gz.updateScreen(getWidth(),getHeight());
+				sun.resize(getWidth(),getHeight());
+				Point.update();
 			}
 		});
 		
@@ -198,112 +174,141 @@ public class World extends JPanel {
 		new AutostartThread(new Runnable() {
 			public void run() {
 				while(true) {
-					if(isActiv) {
+					if(isActiv && msTimeout == 0)
 						step();
-					}else {
-						try {
-							Thread.sleep(1000);
-						} catch (InterruptedException e) {
-							e.printStackTrace();
-						}
-					}
-					
-					if(msTimeout != 0) {
-						Utils.pause_ms(msTimeout);
-					}
+					else
+						Utils.pause(1);
 				}
 			}
 		});
 		
 	}
-
-	private void newSize(int w, int h) {
-		double hDel = 1.0 * h * (1 - (UP_border + DOWN_border)) / (MAP_CELLS.height);
-		double wDel = 1.0 * w / (MAP_CELLS.width);
-		scale = Math.min(hDel, wDel);
-		border.width = (int) Math.round((w -MAP_CELLS.width*scale)/2);
-		border.height = (int) Math.round((h - MAP_CELLS.height*scale)/2);
-	}
 	
+	private void worldGenerate() {
+		geysers = new Geyser[3];
+		geysers[0]  = new Geyser(0,World.MAP_CELLS.width * 3 / 40,getWidth(),getHeight(),DIRECTION.DOWN,10);
+		geysers[1]  = new Geyser(World.MAP_CELLS.width * 3 / 40,World.MAP_CELLS.width * 6 / 40,getWidth(),getHeight(),DIRECTION.UP,10);
+		geysers[2]  = new Geyser(World.MAP_CELLS.width * 6 / 40,World.MAP_CELLS.width * 9 / 40,getWidth(),getHeight(),DIRECTION.DOWN,10);
+		sun = new Sun(getWidth(),getHeight());
+		Point.update();
+		
+		int countProc = Runtime.getRuntime().availableProcessors()*2/3;
+		while(MAP_CELLS.width - 2*countProc < countProc)
+			countProc--;
+		
+		/**Сколько рядов уместится в одном процессоре*/
+		int lenghtX = MAP_CELLS.width / countProc;
+		/**Сколько рядов придётся ещё добавить*/
+		int addX = MAP_CELLS.width - lenghtX*countProc;
+		
+		Point [][] cellsFirst = new Point[countProc][(lenghtX/2) * MAP_CELLS.height];
+		Point [][] cellsSecond = new Point[countProc][(lenghtX/2 + (lenghtX%2!=0?1:0)) * MAP_CELLS.height];
+		Point [] cellsAdd = new Point[addX * MAP_CELLS.height];
+		
+		for (int proc = 0; proc < countProc; proc++) {
+			for (int x = 0; x < lenghtX; x++) {
+				for (int y = 0; y < MAP_CELLS.height; y++) {
+					Point point = new Point(x + proc * lenghtX,y);
+					if(x >= lenghtX/2 ) {
+						cellsSecond[proc][(x - lenghtX/2)*MAP_CELLS.height+y] = point;
+					} else {
+						cellsFirst[proc][x*MAP_CELLS.height+y] = point;
+					}
+				}
+			}
+		}
+		
+		for (int i = 0; i < addX; i++) {
+			for (int y = 0; y < MAP_CELLS.height; y++) {
+				cellsAdd[i*MAP_CELLS.height+y] = new Point(countProc * lenghtX + i,y);
+			}
+		}
+		
+		cellsTask = new WorldTask[cellsFirst.length];
+		for (int i = 0; i < cellsTask.length; i++)
+			cellsTask[i] = new WorldTask(cellsFirst[i],cellsSecond[i]);
+		if(cellsAdd.length != 0)
+			cellsTask[cellsTask.length-1].cellsSecond = Utils.concat(cellsTask[cellsTask.length-1].cellsSecond, cellsAdd);
+		threads = new Thread[cellsTask.length];
+		
+		recalculate();
+
+		this.settings.updateScrols();
+	}
+
 	public void step() {
 		try {
-			if (Math.random() <= 0.5) {
-				Thread thread = new AutostartThread(betweenCells.start(null));
-				thread.join();
-				for (int i = 0; i < threads.length; i++)
-					threads[i] = new AutostartThread(cells[i].start(null));
-				for (Thread i : threads)
-					i.join();
-			} else {
-				for (int i = 0; i < threads.length; i++)
-					threads[i] = new AutostartThread(cells[i].start(null));
-				for (Thread i : threads)
-					i.join();
-				Thread thread = new AutostartThread(betweenCells.start(null));
-				thread.join();
-			}
-			for (MegaCell megaCell : megaCells) {
-				megaCell.step();
-			}
+			boolean flag = Math.random() <= 0.5;
+			for (int i = 0; i < threads.length; i++)
+				threads[i] = new AutostartThread(cellsTask[i].start(flag));
+			for (Thread i : threads)
+				i.join();
+			flag = !flag;
+			for (int i = 0; i < threads.length; i++)
+				threads[i] = new AutostartThread(cellsTask[i].start(flag));
+			for (Thread i : threads)
+				i.join();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		sun.step(step);
 
 		step++;
 		sps.interapt();
 	}
 
 	public void paintComponent(Graphics g) {
-		repaint();
-	
-		if(!isActiv || (step % FPS_TIC == 0)) {
-			super.paintComponent(g);
-			
-			paintField(g);
-			
-			int localCl = 0,localCO = 0;
-			for (Cell[] cell : worldMap) {
-				for (Cell cell2 : cell) {
-					if(cell2 != null) {
-						cell2.paint(g);
-						if(cell2.alive == LV_STATUS.LV_ALIVE)
-							localCl++;
-						else
-							localCO++;
-					}
+		super.paintComponent(g);
+		
+		paintField(g);
+		
+		if(isActiv && (msTimeout != 0 && timeoutStep % msTimeout == 0)) {
+			step();
+		}
+		int localCl = 0,localCO = 0;
+		for (CellObject[] cell : worldMap) {
+			for (CellObject cell2 : cell) {
+				if(cell2 != null) {
+					cell2.paint(g);
+					if(cell2.aliveStatus(LV_STATUS.LV_ALIVE))
+						localCl++;
+					else
+						localCO++;
 				}
 			}
-			countLife = localCl;
-			countOrganic = localCO;
-			if(info.getCell() != null) {
-				g.setColor(Color.GRAY);
-				g.drawLine(info.getCell().pos.getRx(), border.height, info.getCell().pos.getRx(), getHeight()-border.height);
-				g.drawLine(border.width, info.getCell().pos.getRy(), getWidth()-border.width, info.getCell().pos.getRy());
-			}
 		}
-
+		countLife = localCl;
+		countOrganic = localCO;
+		if(info.getCell() != null) {
+			g.setColor(Color.GRAY);
+			g.drawLine(info.getCell().getPos().getRx(), border.height, info.getCell().getPos().getRx(), getHeight()-border.height);
+			g.drawLine(border.width, info.getCell().getPos().getRy(), getWidth()-border.width, info.getCell().getPos().getRy());
+		}
+		
+		timeoutStep++;
 		fps.interapt();
+		repaint();
 	}
 
 	private void paintField(Graphics g) {
+		//Небо
 		g.setColor(colors[0]);
 		g.fillRect(border.width, 0, getWidth()-border.width*2, border.height);
-
+		//Вода
 		int lenghtY = getHeight()-border.height*2;
-        int heightSun = (int) Math.ceil(lenghtY/World.DIRTY_WATER);
-        for (int i = 0; i < World.DIRTY_WATER; i++) {
-    		g.setColor(colors[1+i]);
-    		g.fillRect(border.width, border.height + i*heightSun, getWidth()-border.width*2, heightSun);
-		}
-  
-		g.setColor( colors[(int) (1+World.DIRTY_WATER)]);
+		sun.paint(g);
+        //Минералы
+		g.setColor( colors[1]);
 		g.fillRect(border.width, (int) (border.height + lenghtY*LEVEL_MINERAL), getWidth()-border.width*2, (int) (lenghtY*(1-LEVEL_MINERAL)/2));
-		g.setColor( colors[(int) (1+World.DIRTY_WATER+1)]);
+		g.setColor( colors[1+1]);
 		g.fillRect(border.width, (int) (getHeight()-border.height-(lenghtY*(1-LEVEL_MINERAL)/2)), getWidth()-border.width*2,(int) (lenghtY*(1-LEVEL_MINERAL)/2));
-		
-		g.setColor(colors[(int) (1+World.DIRTY_WATER+2)]);
+		//Гейзеры
+		for(Geyser gz : geysers)
+			gz.paint(g);
+
+		//Земля
+		g.setColor(colors[1+2]);
 		g.fillRect(border.width, getHeight()-border.height, getWidth()-border.width*2, border.height);
-				
 		//paintLine(g);
 	}
 
@@ -326,10 +331,10 @@ public class World extends JPanel {
 	 * Тасует вектор в случайном порядке
 	 * @param array
 	 */
-	private void shuffle(Object[] array) {
+	private <T> void  shuffle(T[] array) {
 		for (int i = array.length - 1; i > 0; i--) {
 			int j = (int) Math.floor(Math.random() * (i + 1)); // случайный индекс от 0 до i
-			Object t = array[i];
+			T t = array[i];
 			array[i] = array[j];
 			array[j] = t;
 		}
@@ -338,38 +343,33 @@ public class World extends JPanel {
 	public OBJECT test(Point point) {
 		if(point.y < 0 || point.y >= MAP_CELLS.height)
 			return OBJECT.WALL;
-		Cell cell = get(point);
-		if(cell == null)
+		CellObject cell = get(point);
+		if(cell == null || cell.aliveStatus(LV_STATUS.GHOST))
 			return OBJECT.CLEAN;
-		else if(cell.alive == LV_STATUS.LV_ORGANIC_HOLD || cell.alive == LV_STATUS.LV_ORGANIC_SINK)
+		else if(cell.aliveStatus(LV_STATUS.LV_ORGANIC))
 			return OBJECT.ORGANIC;
 		else
 			return OBJECT.BOT;
 	}
 
-	public Cell get(Point point) {
+	public CellObject get(Point point) {
 		return worldMap[point.x][point.y];
 	}
 	
-	void add(Cell cell) {
-		worldMap[cell.pos.x][cell.pos.y] = cell;
+	public void add(CellObject cell) {
+		worldMap[cell.getPos().x][cell.getPos().y] = cell;
 	}
 	public void clean(Point point) {
 		worldMap[point.x][point.y] = null;
 	}
 
 	public void recalculate() {
-		colors = new Color[(int) (2 + World.DIRTY_WATER+2)];
-		colors[0] = new Color(224, 255, 255, 255); //небо
-		for (int i = 0; i < World.DIRTY_WATER; i++) {
-			float sunPower = (float) ((240 - Math.max(0, (1.0*World.SUN_POWER - i)/World.SUN_POWER)*60)/360);
-			colors[1+i] = Utils.getHSBColor(sunPower, 1, 1,0.5);
-		}
-
-		colors[(int) (1 + World.DIRTY_WATER)] = Utils.getHSBColor(270.0/360,0.5,1.0,0.5);
-		colors[(int) (1 + World.DIRTY_WATER+1)] =  ( Utils.getHSBColor(300.0/360,0.5,1.0,0.5));
-		colors[(int) (1 + World.DIRTY_WATER+2)] = new Color(139, 69, 19, 255);
-		
+		colors = new Color[2 + 2];
+		colors[0] = new Color(224, 255, 255, 255); // небо
+		colors[1] = Utils.getHSBColor(270.0 / 360, 0.5, 1.0, 0.5);
+		colors[1 + 1] = (Utils.getHSBColor(300.0 / 360, 0.5, 1.0, 0.5));
+		colors[1 + 2] = new Color(139, 69, 19, 255);
+		sun.resize(getWidth(),getHeight());
 	}
 
 	public JSONmake serelization() {
@@ -382,16 +382,15 @@ public class World extends JPanel {
 		configWorld.add("AGGRESSIVE_ENVIRONMENT", AGGRESSIVE_ENVIRONMENT);
 		configWorld.add("LEVEL_MINERAL", LEVEL_MINERAL);
 		configWorld.add("CONCENTRATION_MINERAL", CONCENTRATION_MINERAL);
-		configWorld.add("FPS_TIC", FPS_TIC);
 		configWorld.add("TIK_TO_EXIT", TIK_TO_EXIT);
 		configWorld.add("step", step);
 		make.add("configWorld", configWorld);
 		
 		make.add("EvoTree", tree.toJSON());
 		System.out.println("EvoTree готово");
-		Vector<Cell> cells = new Vector<>();
-		for (Cell[] cell : worldMap) {
-			for (Cell cell2 : cell) {
+		Vector<CellObject> cells = new Vector<>();
+		for (CellObject[] cell : worldMap) {
+			for (CellObject cell2 : cell) {
 				if(cell2 != null) {
 					cells.add(cell2);
 				}
@@ -419,26 +418,47 @@ public class World extends JPanel {
 		AGGRESSIVE_ENVIRONMENT = configWorld.getD("AGGRESSIVE_ENVIRONMENT");
 		LEVEL_MINERAL = configWorld.getD("LEVEL_MINERAL");
 		CONCENTRATION_MINERAL = configWorld.getD("CONCENTRATION_MINERAL");
-		FPS_TIC = configWorld.getI("FPS_TIC");
 		TIK_TO_EXIT = configWorld.getI("TIK_TO_EXIT");
 		step = configWorld.getL("step");
 		
 		tree = new EvolutionTree(jsoNmake.getJ("EvoTree"));
 		
 		List<JSONmake> cells = jsoNmake.getAJ("Cells");		
-		worldMap = new Cell[MAP_CELLS.width][MAP_CELLS.height];
-		for (JSONmake cell : cells) 
-	    	add(new Cell(cell,tree));
+		worldMap = new CellObject[MAP_CELLS.width][MAP_CELLS.height];
+		for (JSONmake cell : cells) {
+			switch (LV_STATUS.values()[cell.getI("alive")]) {
+				case LV_ALIVE : {
+			    	//Point pos = new Point(cell.getJ("pos"));
+					//if(pos.x == MAP_CELLS.width/2 && (pos.y > 100 && pos.y < 120))
+						add(new AliveCell(cell,tree));
+				}break;
+				case LV_ORGANIC:
+					add(new Organic(cell));
+					break;
+				default:
+					System.err.println(cell);
+			}
+			
+			
+		}
 		//Когда все сохранены, обновялем список друзей
 		for (JSONmake cell : cells) {
 	    	Point pos = new Point(cell.getJ("pos"));
+	    	CellObject realCell = get(pos);
+	    	if(realCell == null || !(realCell instanceof AliveCell))
+	    		continue;
+
 	    	List<JSONmake> mindL = cell.getAJ("friends");
-	    	Cell realCell = get(pos);
+			AliveCell new_name = (AliveCell) realCell;
 	    	for (JSONmake pointFriend : mindL) {
 	    		pos = new Point(pointFriend);
-	    		realCell.setFriend(get(pos));
+	    		if (get(pos) instanceof AliveCell)
+		    		new_name.setFriend((AliveCell)get(pos));
 			}
 		}
+		
+		tree.updatre();
+		
 		settings.updateScrols();
 	}
 }
