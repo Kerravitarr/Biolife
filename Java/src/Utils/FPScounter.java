2a8724aa617b875d9f5b2fe33c119c0fa3a1fd3c
Ilia Pushkin <rjhjkm20@mail.ru>
2022-03-19 13:33:09 +0300

    Update FPScounter.java
    Раньше каждый счётчик запускал свой таймер. Но это жутко не удобно, к тому-же тратит много ресурсов. Теперь на всех один процессор!

--- Java/src/Utils/FPScounter.java
+++ Java/src/Utils/FPScounter.java
@@ -1,19 +1,32 @@
 package Utils;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class FPScounter {
 	int counterFrame = 0;
 	double k = 0.5;  // коэффициент фильтрации, 0.0-1.0
-	private double FPS = 0;
+	private double FPS = 0;	
+	private static List<FPScounter> listeners = new ArrayList<>();
+	
+	private static Timer timer = new Timer("FPScounter");
+	static {
+		timer.schedule(new TimerTask() {
+			public void run() {
+				listeners.forEach(counter -> {
+					counter.updateDPS();
+				});
+			}
+		}, 0L, 1000);
+	}
 
 	public FPScounter() {
-		new Thread() {
-			public void run() {
-				while(true) {
-					updateDPS();
-					Utils.pause(1);
-				}
-			}
-		}.start();
+		listeners.add(this);
+	}
+	public void close() {
+		listeners.remove(this);
 	}
 	
 	public void interapt() {
