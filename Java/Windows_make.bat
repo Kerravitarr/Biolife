74ec26d5436514cc80b44b83d64668db31228369
Ilia Pushkin <rjhjkm20@mail.ru>
2022-03-22 08:05:05 +0300

    Create Windows_make.bat
    Батник сборки/очистки проекта для винды

---
+++ Java/Windows_make.bat
@@ -0,0 +1,24 @@
+@echo off
+choice /C IC /D I /T 5 /M "I - build and install; C - clean"
+
+if %errorlevel%==1 Goto make
+if %errorlevel%==2 Goto clean
+
+:make
+javac -encoding utf8 -d bin -classpath libs\json-simple-1.1.1.jar src\start\*.java src\main\*.java src\start\*.java src\MapObjects\*.java src\panels\*.java src\Utils\*.java
+
+javadoc -encoding utf8  -d docs -classpath libs\json-simple-1.1.1.jar -sourcepath src\start\*.java src\main\*.java src\start\*.java src\MapObjects\*.java src\panels\*.java src\Utils\*.java
+
+jar -cef start.BioLife BioLife.jar -C bin .
+
+mkdir .\build\
+move BioLife.jar .\build\
+
+goto end
+
+:clean
+rd /s /q .\build\
+rd /s /q .\docs\
+rd /s /q .\bin\
+
+:end:
