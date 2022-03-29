fa8abd2be375f3668e4dcd6b09e6bcf0495793e7
Ilia Pushkin <rjhjkm20@mail.ru>
2022-03-29 17:20:58 +0300

    Update Settings.java
    Тоже переехали на универсальный класс, описывающий скрол с подписью

--- Java/src/panels/Settings.java
+++ Java/src/panels/Settings.java
@@ -1,6 +1,9 @@
 package panels;
 
+import java.awt.Adjustable;
 import java.awt.BorderLayout;
+import java.awt.event.AdjustmentEvent;
+import java.awt.event.AdjustmentListener;
 import java.awt.event.ComponentEvent;
 import java.io.FileReader;
 import java.io.FileWriter;
@@ -27,23 +30,93 @@ import main.Configurations;
 import main.World;
 
 public class Settings extends JPanel{
-	private JScrollBar scrollBar_7;
-	private JScrollBar scrollBar_6;
-	private JScrollBar scrollBar_5;
-	private JScrollBar scrollBar_4;
-	private JScrollBar scrollBar_2;
-	private JScrollBar scrollBar_1;
-	private JScrollBar const_SP;
-	public JScrollBar scale;
+	public static class ScrollPanel extends JPanel {
+		/**Описание ползунка*/
+		private JLabel label;
+		/**Скролик*/
+		private JScrollBar scroll;
+		/**Показывает, что отсчёт ведётся в обратную сторону*/
+		private boolean isBack;
+		
+		public ScrollPanel(String text, int min, int max){
+			setLayout(new BorderLayout(0, 0));
+			label = new JLabel(text);
+			label.setHorizontalAlignment(SwingConstants.CENTER);
+			add(label, BorderLayout.NORTH);
+			
+			isBack = min > max;
+			
+			scroll = new JScrollBar();
+			scroll.setVisibleAmount (0); // Значение экстента равно 0
+			if(isBack) {
+				scroll.setMinimum(max);
+				scroll.setMaximum(min);
+			}else {
+				scroll.setMinimum(min);
+				scroll.setMaximum(max);
+			}
+			scroll.setOrientation(JScrollBar.HORIZONTAL);
+			add(scroll, BorderLayout.SOUTH);
+		}
+
+		public void setValue(int val) {
+			setToolTipText("Значение: "  + String.valueOf(val));
+			if(isBack)
+				val = getMaximum() - val;
+			scroll.setValue(val);
+		}
+		public void setValue(double val) {
+			setValue((int)Math.round(val));
+		}
+
+		public void setMaximum(int max) {
+			scroll.setMaximum(max);
+		}
+
+		public int getMaximum() {
+			return scroll.getMaximum();
+		}
+
+		public void setBlockIncrement(int blockIncrement) {
+			scroll.setBlockIncrement(blockIncrement);
+		}
+
+		public void addAdjustmentListener(AdjustmentListener listener) {
+			scroll.addAdjustmentListener(e->{
+				if(isBack)
+					e = new AdjustmentEvent((Adjustable) e.getSource(),e.getID(),e.getAdjustmentType(),(getMaximum() - e.getValue())+scroll.getMinimum());
+				setToolTipText("Значение: "  + String.valueOf(e.getValue()));
+				listener.adjustmentValueChanged(e);
+			});
+		}
+		 public void setToolTipText(String text) {
+			 super.setToolTipText(text);
+			 label.setToolTipText(text);
+			 scroll.setToolTipText(text);
+		 }
+
+		public int getValue() {
+			return scroll.getValue();
+		}
+	}
+	
+	private ScrollPanel scrollBar_7;
+	private ScrollPanel scrollBar_6;
+	private ScrollPanel scrollBar_5;
+	private ScrollPanel scrollBar_4;
+	private ScrollPanel scrollBar_2;
+	private ScrollPanel scrollBar_1;
+	private ScrollPanel const_SP;
+	public ScrollPanel scale;
 	JComponent listener = null;
 	private JButton play;
-	private JScrollBar sun_speed;
-	private JScrollBar scroll_SP;
+	private ScrollPanel sun_speed;
+	private ScrollPanel scroll_SP;
 	private JButton load_button;
 	private JButton saveButton;
 	private JButton step_button;
-	private JScrollBar sun_size;
-	private JScrollBar PoisonStreem;
+	private ScrollPanel sun_size;
+	private ScrollPanel PoisonStreem;
 	
 	/**
 	 * Create the panel.
@@ -58,89 +131,81 @@ public class Settings extends JPanel{
 		JPanel panel = new JPanel();
 		add(panel, BorderLayout.CENTER);
 		
-		JPanel panel_1 = new JPanel();
-		
-		JPanel panel_2 = new JPanel();
-		
-		JPanel panel_3 = new JPanel();
+		const_SP = new ScrollPanel("Постоянная освещённость",1,30);
+		scroll_SP = new ScrollPanel("Движущаяся освещённость",1,30);
+		scrollBar_1 = new ScrollPanel("Загрязнённость воды",1,50);
+		scrollBar_2 = new ScrollPanel("Мутагенность",0,100);
+		scrollBar_4 = new ScrollPanel("Высота минерализации",0,100);
+		scrollBar_5 = new ScrollPanel("Концентрация минералов",0,40);
+		scrollBar_5.setBlockIncrement(5);
+		scrollBar_6 = new ScrollPanel("Скорость разложения",10,1);
+		scrollBar_7 = new ScrollPanel("Частота кадров",1000,0);
+		scrollBar_7.setBlockIncrement(20);
+		scale = new ScrollPanel("Масштаб",10,100);
+		scale.setValue(10);
+		sun_speed = new ScrollPanel("Скорость солнца",100,1);
+		sun_speed.setValue(Configurations.SUN_SPEED);
+		sun_size = new ScrollPanel("Размер солнца",1,Configurations.SUN_PARTS*2);
+		PoisonStreem = new ScrollPanel("Вязкость яда",1,16);
+		PoisonStreem.setBlockIncrement(3);
+		PoisonStreem.setValue((int) Math.round(Math.log(Configurations.POISON_STREAM)));
 		
 		play = new JButton();
-		
-		JPanel panel_5 = new JPanel();
-		
-		JPanel panel_6 = new JPanel();
-		
 		saveButton = new JButton("Сохранить мир");
-		
 		load_button = new JButton("Загрузить мир");
-		
 		step_button = new JButton("Шаг");
 		step_button.addActionListener(e-> Configurations.world.step());
 		
-		JPanel panel_4_1 = new JPanel();
-		panel_4_1.setLayout(new BorderLayout(0, 0));
-		
-		JPanel panel_7 = new JPanel();
-		
-		JPanel panel_8 = new JPanel();
-		
-		JPanel panel_4 = new JPanel();
-		
-		JPanel panel_9 = new JPanel();
-		
-		JPanel panel_10 = new JPanel();
-		
-		JPanel panel_11 = new JPanel();
 		GroupLayout gl_panel = new GroupLayout(panel);
 		gl_panel.setHorizontalGroup(
 			gl_panel.createParallelGroup(Alignment.TRAILING)
 				.addGroup(gl_panel.createSequentialGroup()
 					.addContainerGap()
 					.addGroup(gl_panel.createParallelGroup(Alignment.TRAILING)
-						.addComponent(panel_11, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 177, Short.MAX_VALUE)
-						.addComponent(panel_1, GroupLayout.DEFAULT_SIZE, 177, Short.MAX_VALUE)
+						.addComponent(PoisonStreem, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 177, Short.MAX_VALUE)
+						.addComponent(const_SP, GroupLayout.DEFAULT_SIZE, 177, Short.MAX_VALUE)
 						.addComponent(saveButton, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 177, Short.MAX_VALUE)
 						.addComponent(load_button, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 177, Short.MAX_VALUE)
 						.addComponent(play, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 177, Short.MAX_VALUE)
 						.addComponent(step_button, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 177, Short.MAX_VALUE)
-						.addComponent(panel_7, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 177, Short.MAX_VALUE)
-						.addComponent(panel_4_1, GroupLayout.DEFAULT_SIZE, 177, Short.MAX_VALUE)
-						.addComponent(panel_3, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 177, Short.MAX_VALUE)
-						.addComponent(panel_5, GroupLayout.DEFAULT_SIZE, 177, Short.MAX_VALUE)
-						.addComponent(panel_6, GroupLayout.DEFAULT_SIZE, 177, Short.MAX_VALUE)
-						.addComponent(panel_10, GroupLayout.DEFAULT_SIZE, 177, Short.MAX_VALUE)
-						.addComponent(panel_4, GroupLayout.DEFAULT_SIZE, 177, Short.MAX_VALUE)
-						.addComponent(panel_2, GroupLayout.DEFAULT_SIZE, 177, Short.MAX_VALUE)
-						.addComponent(panel_9, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 177, Short.MAX_VALUE)
-						.addComponent(panel_8, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 177, Short.MAX_VALUE))
+						.addComponent(scrollBar_7, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 177, Short.MAX_VALUE)
+						.addComponent(scrollBar_6, GroupLayout.DEFAULT_SIZE, 177, Short.MAX_VALUE)
+						.addComponent(scrollBar_2, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 177, Short.MAX_VALUE)
+						.addComponent(scrollBar_4, GroupLayout.DEFAULT_SIZE, 177, Short.MAX_VALUE)
+						.addComponent(scrollBar_5, GroupLayout.DEFAULT_SIZE, 177, Short.MAX_VALUE)
+						.addComponent(sun_size, GroupLayout.DEFAULT_SIZE, 177, Short.MAX_VALUE)
+						.addComponent(sun_speed, GroupLayout.DEFAULT_SIZE, 177, Short.MAX_VALUE)
+						.addComponent(scrollBar_1, GroupLayout.DEFAULT_SIZE, 177, Short.MAX_VALUE)
+						.addComponent(scroll_SP, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 177, Short.MAX_VALUE)
+						.addComponent(scale, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 177, Short.MAX_VALUE))
 					.addContainerGap())
 		);
 		gl_panel.setVerticalGroup(
 			gl_panel.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_panel.createSequentialGroup()
-					.addComponent(panel_1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+					.addComponent(const_SP, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
 					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(panel_9, GroupLayout.PREFERRED_SIZE, 30, GroupLayout.PREFERRED_SIZE)
+					.addComponent(scroll_SP, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
 					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(panel_2, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+					.addComponent(scrollBar_1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
 					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(panel_4, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+					.addComponent(sun_speed, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
 					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(panel_10, GroupLayout.PREFERRED_SIZE, 29, GroupLayout.PREFERRED_SIZE)
+					.addComponent(sun_size, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
 					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(panel_6, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+					.addComponent(scrollBar_5, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
 					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(panel_5, GroupLayout.PREFERRED_SIZE, 35, GroupLayout.PREFERRED_SIZE)
+					.addComponent(scrollBar_4, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
 					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(panel_3, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+					.addComponent(scrollBar_2, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
 					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(panel_4_1, GroupLayout.PREFERRED_SIZE, 34, GroupLayout.PREFERRED_SIZE)
+					.addComponent(scrollBar_6, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
 					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(panel_11, GroupLayout.PREFERRED_SIZE, 28, GroupLayout.PREFERRED_SIZE)
+					.addComponent(PoisonStreem, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
 					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(panel_7, GroupLayout.PREFERRED_SIZE, 29, GroupLayout.PREFERRED_SIZE)
+					.addComponent(scrollBar_7, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
 					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(panel_8, GroupLayout.PREFERRED_SIZE, 28, GroupLayout.PREFERRED_SIZE)
+					.addComponent(scale, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
 					.addPreferredGap(ComponentPlacement.RELATED, 129, Short.MAX_VALUE)
 					.addComponent(step_button)
 					.addPreferredGap(ComponentPlacement.RELATED)
@@ -151,145 +216,7 @@ public class Settings extends JPanel{
 					.addComponent(saveButton)
 					.addContainerGap())
 		);
-		panel_11.setLayout(new BorderLayout(0, 0));
 		
-		JLabel lblNewLabel_12 = new JLabel("Вязкость яда");
-		lblNewLabel_12.setHorizontalAlignment(SwingConstants.CENTER);
-		panel_11.add(lblNewLabel_12, BorderLayout.NORTH);
-		
-		PoisonStreem = new JScrollBar();
-		PoisonStreem.setBlockIncrement(3);
-		PoisonStreem.setVisibleAmount (0); // Значение экстента равно 0
-		PoisonStreem.setMaximum(16);
-		PoisonStreem.setValue((int) Math.round(Math.log(Configurations.POISON_STREAM)));
-		PoisonStreem.setOrientation(JScrollBar.HORIZONTAL);
-		panel_11.add(PoisonStreem, BorderLayout.SOUTH);
-		panel_10.setLayout(new BorderLayout(0, 0));
-		
-		JLabel lblNewLabel_11 = new JLabel("Размер солнца");
-		lblNewLabel_11.setHorizontalAlignment(SwingConstants.CENTER);
-		panel_10.add(lblNewLabel_11, BorderLayout.NORTH);
-		
-		sun_size = new JScrollBar();
-		sun_size.setVisibleAmount (0); // Значение экстента равно 0
-		sun_size.setMinimum(1);
-		sun_size.setMaximum(Configurations.SUN_PARTS*2);
-		sun_size.setOrientation(JScrollBar.HORIZONTAL);
-		panel_10.add(sun_size, BorderLayout.SOUTH);
-		panel_9.setLayout(new BorderLayout(0, 0));
-		
-		JLabel lblNewLabel_10 = new JLabel("Движущаяся освещённость");
-		lblNewLabel_10.setHorizontalAlignment(SwingConstants.CENTER);
-		panel_9.add(lblNewLabel_10, BorderLayout.NORTH);
-		
-		scroll_SP = new JScrollBar();
-		scroll_SP.setVisibleAmount (0); // Значение экстента равно 0
-		scroll_SP.setMinimum(1);
-		scroll_SP.setMaximum(30);
-		scroll_SP.setOrientation(JScrollBar.HORIZONTAL);
-		panel_9.add(scroll_SP, BorderLayout.SOUTH);
-		panel_4.setLayout(new BorderLayout(0, 0));
-		
-		JLabel lblNewLabel_4 = new JLabel("Скорость солнца");
-		lblNewLabel_4.setHorizontalAlignment(SwingConstants.CENTER);
-		panel_4.add(lblNewLabel_4, BorderLayout.NORTH);
-		
-		sun_speed = new JScrollBar();
-		sun_speed.setVisibleAmount (0);
-		sun_speed.setMinimum(1);
-		sun_speed.setOrientation(JScrollBar.HORIZONTAL);
-		sun_speed.setValue(sun_speed.getMaximum() - Configurations.SUN_SPEED + 1);
-		panel_4.add(sun_speed, BorderLayout.SOUTH);
-		panel_8.setLayout(new BorderLayout(0, 0));
-		
-		JLabel lblNewLabel_9 = new JLabel("Масштаб");
-		lblNewLabel_9.setHorizontalAlignment(SwingConstants.CENTER);
-		panel_8.add(lblNewLabel_9, BorderLayout.NORTH);
-		
-		scale = new JScrollBar();
-		scale.setValue(10);
-		scale.setMinimum(10);
-		scale.setOrientation(JScrollBar.HORIZONTAL);
-		panel_8.add(scale, BorderLayout.SOUTH);
-		panel_7.setLayout(new BorderLayout(0, 0));
-		
-		JLabel lblNewLabel_8 = new JLabel("Частота кадров");
-		lblNewLabel_8.setHorizontalAlignment(SwingConstants.CENTER);
-		panel_7.add(lblNewLabel_8, BorderLayout.NORTH);
-		
-		scrollBar_7 = new JScrollBar();
-		scrollBar_7.setVisibleAmount (0); // Значение экстента равно 0
-		scrollBar_7.setBlockIncrement(20);
-		scrollBar_7.setMaximum(1000);
-		scrollBar_7.setMinimum(1);
-		scrollBar_7.setOrientation(JScrollBar.HORIZONTAL);
-		panel_7.add(scrollBar_7, BorderLayout.SOUTH);
-		
-		JLabel lblNewLabel_7 = new JLabel("Скорость разложения");
-		lblNewLabel_7.setHorizontalAlignment(SwingConstants.CENTER);
-		panel_4_1.add(lblNewLabel_7, BorderLayout.NORTH);
-		
-		scrollBar_6 = new JScrollBar();
-		scrollBar_6.setVisibleAmount (0); // Значение экстента равно 0
-		scrollBar_6.setMaximum(10);
-		scrollBar_6.setOrientation(JScrollBar.HORIZONTAL);
-		panel_4_1.add(scrollBar_6, BorderLayout.SOUTH);
-		panel_6.setLayout(new BorderLayout(0, 0));
-		
-		JLabel lblNewLabel_6 = new JLabel("Концентрация минералов");
-		lblNewLabel_6.setHorizontalAlignment(SwingConstants.CENTER);
-		panel_6.add(lblNewLabel_6, BorderLayout.NORTH);
-		
-		scrollBar_5 = new JScrollBar();
-		scrollBar_5.setMaximum(40);
-		scrollBar_5.setBlockIncrement(5);
-		scrollBar_5.setVisibleAmount (0); // Значение экстента равно 0
-		scrollBar_5.setOrientation(JScrollBar.HORIZONTAL);
-		panel_6.add(scrollBar_5, BorderLayout.SOUTH);
-		panel_5.setLayout(new BorderLayout(0, 0));
-		
-		JLabel lblNewLabel_5 = new JLabel("Высота минерализации");
-		lblNewLabel_5.setHorizontalAlignment(SwingConstants.CENTER);
-		panel_5.add(lblNewLabel_5, BorderLayout.NORTH);
-		
-		scrollBar_4 = new JScrollBar();
-		scrollBar_4.setVisibleAmount (0); // Значение экстента равно 0
-		scrollBar_4.setOrientation(JScrollBar.HORIZONTAL);
-		panel_5.add(scrollBar_4, BorderLayout.SOUTH);
-		panel_3.setLayout(new BorderLayout(0, 0));
-		
-		JLabel lblNewLabel_3 = new JLabel("Мутагенность");
-		lblNewLabel_3.setHorizontalAlignment(SwingConstants.CENTER);
-		panel_3.add(lblNewLabel_3, BorderLayout.NORTH);
-		
-		scrollBar_2 = new JScrollBar();
-		scrollBar_2.setVisibleAmount (0); // Значение экстента равно 0
-		scrollBar_2.setOrientation(JScrollBar.HORIZONTAL);
-		panel_3.add(scrollBar_2, BorderLayout.SOUTH);
-		panel_2.setLayout(new BorderLayout(0, 0));
-		
-		JLabel lblNewLabel_2 = new JLabel("Загрязнённость воды");
-		lblNewLabel_2.setHorizontalAlignment(SwingConstants.CENTER);
-		panel_2.add(lblNewLabel_2, BorderLayout.NORTH);
-		
-		scrollBar_1 = new JScrollBar();
-		scrollBar_1.setVisibleAmount (0); // Значение экстента равно 0
-		scrollBar_1.setMinimum(1);
-		scrollBar_1.setMaximum(50);
-		scrollBar_1.setOrientation(JScrollBar.HORIZONTAL);
-		panel_2.add(scrollBar_1, BorderLayout.SOUTH);
-		panel_1.setLayout(new BorderLayout(0, 0));
-		
-		JLabel lblNewLabel_1 = new JLabel("Постоянная освещённость");
-		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
-		panel_1.add(lblNewLabel_1, BorderLayout.NORTH);
-		
-		const_SP = new JScrollBar();
-		const_SP.setVisibleAmount (0); // Значение экстента равно 0
-		const_SP.setMinimum(1);
-		const_SP.setMaximum(30);
-		const_SP.setOrientation(JScrollBar.HORIZONTAL);
-		panel_1.add(const_SP, BorderLayout.SOUTH);
 		panel.setLayout(gl_panel);
 		setListeners();
 	}
@@ -299,20 +226,20 @@ public class Settings extends JPanel{
 			play.setText("Пауза");
 		else
 			play.setText("Пуск");
-		sun_speed.setValue(sun_speed.getMaximum() - Configurations.SUN_SPEED + 1);
+		sun_speed.setValue(Configurations.SUN_SPEED );
 		scroll_SP.setValue(Configurations.ADD_SUN_POWER);
 		sun_size.setMaximum(Configurations.SUN_PARTS*2);
 		if(Configurations.SUN_LENGHT <= Configurations.SUN_PARTS)
 			sun_size.setValue(Configurations.SUN_LENGHT);
 		else
 			sun_size.setValue((int) Math.round(Math.pow(Configurations.SUN_LENGHT-Configurations.SUN_PARTS, 0.5)+Configurations.SUN_PARTS));
-		scrollBar_7.setValue(scrollBar_7.getMaximum() - World.msTimeout);
-		scrollBar_6.setValue(scrollBar_6.getMaximum() - Configurations.TIK_TO_EXIT + 1);
-		scrollBar_5.setValue((int) Math.round(Configurations.CONCENTRATION_MINERAL*10));
-		scrollBar_4.setValue((int)  Math.round((1-Configurations.LEVEL_MINERAL)*100));
-		scrollBar_2.setValue((int) Math.round(Configurations.AGGRESSIVE_ENVIRONMENT*100));
-		scrollBar_1.setValue((int) Math.round(Configurations.DIRTY_WATER));
-		const_SP.setValue((int) Math.round(Configurations.BASE_SUN_POWER));
+		scrollBar_7.setValue(World.msTimeout);
+		scrollBar_6.setValue(Configurations.TIK_TO_EXIT);
+		scrollBar_5.setValue(Configurations.CONCENTRATION_MINERAL*10);
+		scrollBar_4.setValue((1-Configurations.LEVEL_MINERAL)*100);
+		scrollBar_2.setValue(Configurations.AGGRESSIVE_ENVIRONMENT*100);
+		scrollBar_1.setValue(Configurations.DIRTY_WATER);
+		const_SP.setValue(Configurations.BASE_SUN_POWER);
 	}
 	
 	public void setListeners() {
@@ -333,7 +260,7 @@ public class Settings extends JPanel{
 			Configurations.sun.repaint();
 		});
 		sun_speed.addAdjustmentListener(e->{
-			Configurations.SUN_SPEED =  (int) Math.pow(sun_speed.getMaximum() - e.getValue() + 1,1.5);
+			Configurations.SUN_SPEED =  e.getValue();
 		});
 		const_SP.addAdjustmentListener(e->{
 			Configurations.BASE_SUN_POWER = e.getValue();
@@ -355,10 +282,10 @@ public class Settings extends JPanel{
 			Configurations.world.recalculate();
 		});
 		scrollBar_6.addAdjustmentListener(e->{
-			Configurations.TIK_TO_EXIT = scrollBar_6.getMaximum() - e.getValue() + 1;
+			Configurations.TIK_TO_EXIT = e.getValue();
 		});
 		scrollBar_7.addAdjustmentListener(e->{
-			World.msTimeout = scrollBar_7.getMaximum() - e.getValue();
+			World.msTimeout = e.getValue();
 		});
 		scale.addAdjustmentListener(e->{
 			if(listener != null) {
