e4d9de98cb5353e5b653d8f097fe6e1c3b9280f5
Ilia Pushkin <rjhjkm20@mail.ru>
2022-03-29 13:14:03 +0300

    Update Windows_make.bat
    Улучшение батника, теперь он больше помогает при ошибках

--- Java/Windows_make.bat
+++ Java/Windows_make.bat
@@ -1,24 +1,53 @@
-@echo off
-choice /C IC /D I /T 5 /M "I - build and install; C - clean"
 
-if %errorlevel%==1 Goto make
-if %errorlevel%==2 Goto clean
+@choice /C IC /D I /T 5 /M "I - build and install; C - clean"
+
+@if %errorlevel%==1 Goto make
+@if %errorlevel%==2 Goto clean
 
 :make
+@echo =======================================
+@echo STEP 1. Compiling files to bytecode.
+@echo =======================================
 javac -encoding utf8 -d bin src\start\*.java src\main\*.java src\start\*.java src\MapObjects\*.java src\panels\*.java src\Utils\*.java
-
+@if errorlevel 1 Goto error
+@CLS
+@echo =======================================
+@echo Step 1 - OK
+@echo =======================================
+@echo =======================================
+@echo STEP 2. Creation of program documentation
+@echo =======================================
 javadoc -encoding utf8  -d docs -sourcepath src\start\*.java src\main\*.java src\start\*.java src\MapObjects\*.java src\panels\*.java src\Utils\*.java
-
+REM if errorlevel 1 Goto error - Ой, да кому нужны документы? Что-то да соберётся
+@CLS
+@echo =======================================
+@echo Step 2 - OK
+@echo =======================================
+@echo =======================================
+@echo STEP 3. Collecting bytecode into an executable
+@echo =======================================
 jar -cef start.BioLife BioLife.jar -C bin .
+@if errorlevel 1 Goto error
+@CLS
+@echo =======================================
+@echo Step 3 - OK
+@echo =======================================
+
 
 mkdir .\build\
 move BioLife.jar .\build\
-
 goto end
 
 :clean
 rd /s /q .\build\
 rd /s /q .\docs\
 rd /s /q .\bin\
+goto end
+
+:error
+@echo !!!!!!WARNING!!!!!!
+@echo !!!!!!FATAL ERROR!!!!!!
+@echo !!!!!!If you can't figure it out, write to the author!!!!!!
+@pause
 
 :end:
