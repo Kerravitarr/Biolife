6b202291ac7c8b9cbe7d3df6ceb543d6fa93113a
Ilia Pushkin <rjhjkm20@mail.ru>
2022-04-03 21:37:17 +0300

    Update EvolTreeDialog.java
    Поправил одну серьёзную ошибку связанную с параллельной работой - это рисование детей когда их уже нет. Это позволило проверять узел на зрелость и если он труп - ну тогда можно заставить его удалиться ((для надёжности))

--- Java/src/panels/EvolTreeDialog.java
+++ Java/src/panels/EvolTreeDialog.java
@@ -1,5 +1,7 @@
 package panels;
 
+import MapObjects.CellObject;
+import Utils.Utils;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Graphics;
@@ -7,31 +9,28 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-import java.util.Vector;
-
+import java.util.List;
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
 import javax.swing.JDialog;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-
-import MapObjects.CellObject;
-import Utils.Utils;
 import main.Configurations;
 import main.EvolutionTree;
 import main.EvolutionTree.Node;
 import main.Point;
 
 public class EvolTreeDialog extends JDialog {
-	static int delX = 23;
-	static int minX = delX * 2;
-	static int minY = 55;
+	static final int DEL_X = 23;
+	static final int MIN_X = DEL_X * 2;
+	static final int MIN_Y = 60;
 	
 	public class NodeJpanel extends JPanel{
 		Node node = null;
 		NodeJpanel(Node node){
 			this.node = node;
 			addMouseListener(new MouseAdapter() {
+				@Override
 			    public void mouseClicked(MouseEvent e) {
 			    	EvolutionTree.root.setSelected(true);
 			    	NodeJpanel.this.node.setSelected(false);
@@ -41,6 +40,7 @@ public class EvolTreeDialog extends JDialog {
 			});
 		}
 
+		@Override
 		public void paintComponent(Graphics g) {
 			super.paintComponent(g);
 			if(getHeight() >= 10) {
@@ -73,43 +73,60 @@ public class EvolTreeDialog extends JDialog {
 		}
 	
 		
+		@Override
 		public void paintComponent(Graphics g) {
 			if(isNeedUpdate) {
 				isNeedUpdate = false;
-				int maxY = getHeight() - minY;
+				int maxY = getHeight() - MIN_Y;
 
 				removeAll();
-				addNode(EvolutionTree.root,minX,minY,maxY);
+				addNode(EvolutionTree.root,MIN_X,MIN_Y,maxY);
 			}
 			super.paintComponent(g);
 			
-			int maxY = getHeight() - minY;
+			int maxY = getHeight() - MIN_Y;
 			
-			paint(g,EvolutionTree.root,minX,minY,maxY);
-			
+			paint(g,EvolutionTree.root,MIN_X,MIN_Y,maxY);
+			try{
+				printText(g);
+			}catch(java.lang.NullPointerException e){
+				activNode = null;
+			}
+		}
+
+		/**
+		 * Пишет информацию о выбранной клетке на панели
+		 * @param g - панель, куда пишем
+		 * @throws java.lang.NullPointerException - может выбрасывать исключение, когда клетка удаляется 
+		 */
+		private void printText(Graphics g) throws java.lang.NullPointerException{
 			if(activNode != null) {
-				if(activNode.node.countAliveCell() > 0)
-					g.drawString("Примечательные точки узла("+activNode.node.countAliveCell()+"клеток):", minX, 10);
-				else
-					g.drawString("Примечательные точки узла:", minX, 10);
+				if(activNode.node.countAliveCell() > 0){
+					g.drawString("Примечательные точки узла("+activNode.node.countAliveCell()+"клеток):", MIN_X, 10);
+				}else{
+					g.drawString("Примечательные точки узла:", MIN_X, 10);
+					if(activNode.node.getChild().isEmpty())
+						activNode.node.remove();
+				}
 				String DNA_s = "";
-				for(int i : activNode.node.DNA) {
+				for(int i : activNode.node.DNA_mind) {
 					if(!DNA_s.isEmpty())
 						DNA_s+= ", ";
 					DNA_s += i + "";
 				}
-				g.drawString("ДНК (" + activNode.node.DNA.length + "): " + DNA_s, minX, 25);
+				g.drawString("ДНК (" + activNode.node.DNA_mind.length + "): " + DNA_s, MIN_X, 25);
 				Color oldC = g.getColor();
 				g.setColor(activNode.node.phenotype);
 				g.drawString("Фенотип: " + "[r="
 						+ activNode.node.phenotype.getRed() + ",g="
 						+ activNode.node.phenotype.getGreen() + ",b="
-						+ activNode.node.phenotype.getBlue() + "]", minX, 40);
+						+ activNode.node.phenotype.getBlue() + "]", MIN_X, 40);
 				g.setColor(oldC);
-				g.drawString("Устойчивость к яду: " + activNode.node.poisonType, minX, 55);
+				g.drawString("Устойчивость к яду: " + activNode.node.poisonType, MIN_X, 55);
 			}
 		}
 		
+		@Override
 	    public void repaint() {
 			super.repaint();
 			isNeedUpdate = true;
@@ -117,32 +134,32 @@ public class EvolTreeDialog extends JDialog {
 	    private void addNode(Node root, int xPos, int minY, int maxY) {
 	    	NodeJpanel rootJ = new NodeJpanel(root);
 	    	add(rootJ);
-	    	rootJ.setBounds(xPos - delX / 2, minY + (maxY-minY)/2 - delX / 2, Math.min((maxY-minY)/2,delX), Math.min((maxY-minY)/2,delX));
+	    	rootJ.setBounds(xPos - DEL_X / 2, minY + (maxY-minY)/2 - DEL_X / 2, Math.min((maxY-minY)/2,DEL_X), Math.min((maxY-minY)/2,DEL_X));
 	    	
-			Vector<Node> childs = root.getChild();
-			if(childs.size() == 0 || maxY-minY < 10) return;
+			List<Node> childs = root.getChild();
+			if(childs.isEmpty() || maxY-minY < 10) return;
 			double step = (maxY-minY) / childs.size();
 			
 			for(int i = 0 ; i < childs.size() ; i++) {
-				addNode(childs.get(i),xPos + delX,(int)Math.round(minY + step * i),(int)Math.round(minY + step * (i + 1)));
+				addNode(childs.get(i),xPos + DEL_X,(int)Math.round(minY + step * i),(int)Math.round(minY + step * (i + 1)));
 			}
 		}
 	
 		private void paint(Graphics g, Node root, int xPos, int minY, int maxY) {
-			Vector<Node> childs = root.getChild();
-			if(childs.size() == 0) return;
+			List<Node> childs = root.getChild();
+			if(childs.isEmpty()) return;
 			double step = (maxY-minY) / childs.size();
 			
 			for(int i = 0 ; i < childs.size() ; i++) {
-				g.drawLine(xPos, minY + (maxY-minY)/2, xPos + delX, (int)Math.round(minY  +  step * (2 * i + 1)/2));
-				paint(g,childs.get(i),xPos + delX,(int)Math.round(minY + step * i),(int)Math.round(minY + step * (i + 1)));
+				g.drawLine(xPos, minY + (maxY-minY)/2, xPos + DEL_X, (int)Math.round(minY  +  step * (2 * i + 1)/2));
+				paint(g,childs.get(i),xPos + DEL_X,(int)Math.round(minY + step * i),(int)Math.round(minY + step * (i + 1)));
 			}
 		}
 		
 	}
 
 	private static final JPanel contentPanel = new JPanel();
-	private DrawPanelEvoTree draw;
+	private final DrawPanelEvoTree draw;
 	private NodeJpanel activNode = null;;
 
 	/**
@@ -150,11 +167,13 @@ public class EvolTreeDialog extends JDialog {
 	 */
 	public EvolTreeDialog() {
 		addWindowListener(new WindowAdapter() {
+			@Override
 			public void windowDeactivated(WindowEvent e) {
 		    	EvolutionTree.root.setSelected(false);
 		    	CellUpdate();
 	    		activNode = null;
 			}
+			@Override
 		    public void windowActivated(WindowEvent e) {
 		    	EvolutionTree.root.setSelected(true);
 		    	CellUpdate();
@@ -172,6 +191,7 @@ public class EvolTreeDialog extends JDialog {
 		}
 	}
 	
+	@Override
     public void repaint() {
     	super.repaint();
     	draw.repaint();
