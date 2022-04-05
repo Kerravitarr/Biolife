4ccec18056d6dbafa189ee5032152788c9294fa6
Ilia Pushkin <rjhjkm20@mail.ru>
2022-04-05 17:27:07 +0300

    Update Windows_make.bat
    Подобновил сборку, а то давненько не делал этого. Проверил, что всё собирается, а то сливать скоро - а тут косяк на косяке!

--- Java/Windows_make.bat
+++ Java/Windows_make.bat
@@ -8,8 +8,10 @@
 @echo =======================================
 @echo STEP 1. Compiling files to bytecode.
 @echo =======================================
-javac -encoding utf8 -d bin src\start\*.java src\main\*.java src\start\*.java src\MapObjects\*.java src\panels\*.java src\Utils\*.java
+javac -encoding utf8 -d bin src\start\*.java src\main\*.java src\start\*.java src\MapObjects\*.java src\panels\*.java src\Utils\*.java src\MapObjects\dna\*.java
 @if errorlevel 1 Goto error
+mkdir .\bin\locales
+xcopy .\src\locales .\bin\locales /e
 @CLS
 @echo =======================================
 @echo Step 1 - OK
@@ -17,8 +19,8 @@ javac -encoding utf8 -d bin src\start\*.java src\main\*.java src\start\*.java sr
 @echo =======================================
 @echo STEP 2. Creation of program documentation
 @echo =======================================
-javadoc -encoding utf8  -d docs -sourcepath src\start\*.java src\main\*.java src\start\*.java src\MapObjects\*.java src\panels\*.java src\Utils\*.java
-REM if errorlevel 1 Goto error - Ой, да кому нужны документы? Что-то да соберётся
+javadoc -encoding utf8  -d docs -sourcepath src\start\*.java src\main\*.java src\start\*.java src\MapObjects\*.java src\panels\*.java src\Utils\*.java src\MapObjects\dna\*.java
+if errorlevel 1 Goto error - Ой, да кому нужны документы? Что-то да соберётся
 @CLS
 @echo =======================================
 @echo Step 2 - OK
@@ -26,13 +28,12 @@ REM if errorlevel 1 Goto error - Ой, да кому нужны документ
 @echo =======================================
 @echo STEP 3. Collecting bytecode into an executable
 @echo =======================================
-jar -cef start.BioLife BioLife.jar -C bin .
+jar -cvef start.BioLife BioLife.jar -C bin .
 @if errorlevel 1 Goto error
 @CLS
 @echo =======================================
 @echo Step 3 - OK
 @echo =======================================
-
 
 mkdir .\build\
 move BioLife.jar .\build\
@@ -45,6 +46,8 @@ rd /s /q .\bin\
 goto end
 
 :error
+@echo !
+@echo !
 @echo !!!!!!WARNING!!!!!!
 @echo !!!!!!FATAL ERROR!!!!!!
 @echo !!!!!!If you can't figure it out, write to the author!!!!!!
