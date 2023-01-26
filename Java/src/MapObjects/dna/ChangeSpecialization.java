789dddd3f609b774ba23c401d9fe53a2d27aebef
Ilia Pushkin <rjhjkm20@mail.ru>
2023-01-26 17:50:27 +0300

    Смена специализации
    Молодые клетки могут позволить себе поменять специализацию на желаемую

---
+++ Java/src/MapObjects/dna/ChangeSpecialization.java
@@ -0,0 +1,41 @@
+package MapObjects.dna;
+
+import MapObjects.AliveCell;
+import MapObjects.AliveCellProtorype.Specialization;
+
+/**
+ * Смена специализации. Функция доступна только до сотого года жизни!
+ * @author Kerravitarr
+ *
+ */
+public class ChangeSpecialization extends CommandDo {
+
+	public ChangeSpecialization() {super(2);};
+	
+	@Override
+	protected void doing(AliveCell cell) {
+		if(cell.getAge() <= 100) {
+        	int co = param(cell, 0, 100); //Новое значение специализации
+        	int tp = param(cell, 1, Specialization.TYPE.size() - 1); //Какая специализация
+			cell.getSpecialization().set(Specialization.TYPE.staticValues[tp],co);
+		} else {
+			return;
+		}
+	}
+	
+	@Override
+	public String getParam(AliveCell cell, int numParam, DNA dna) {
+		if(cell.getAge() <= 100) {
+			if(numParam == 0) {
+				return Integer.toString(param(dna, 0, 100));
+			} else if(numParam == 1) {
+				var sp = Specialization.TYPE.staticValues[param(dna, 1, Specialization.TYPE.size() - 1)];
+				return isFullMod() ? sp.toString() : sp.toSString();
+			} else {
+				return null;
+			}
+		} else {
+			return "";
+		}
+	}
+}
