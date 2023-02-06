cb216c8ddf28fd6c9196248f86b2c0cc025d80f4
Ilia Pushkin <rjhjkm20@mail.ru>
2023-02-06 17:42:35 +0300

    Новый гейзер
    Теперь без всякой фигни с нагромождением.
    Я просто тогда не знал про градиенты

--- Java/src/MapObjects/Geyser.java
+++ Java/src/MapObjects/Geyser.java
@@ -1,6 +1,7 @@
 package MapObjects;
 
 import java.awt.Color;
+import java.awt.GradientPaint;
 import java.awt.Graphics;
 
 import Utils.ColorRec;
@@ -18,74 +19,44 @@ import main.Point.DIRECTION;
  *
  */
 public class Geyser {
-	/**
-	 * Кусок гейзера. Сам гейзер состоит из разных областей
-	 * Вот это описание одной конкретной области
-	 * @author Илья
-	 *
-	 */
-	private static class Section extends ColorRec{
-		public Section(int x0, int y0, int w, int h, Color c, int startL, int lenghtSection, int power) {
-			super(x0, y0, w, h, c);
-			startX = startL;
-			endX = startX + lenghtSection;
-			this.power=power;
-		}
-		/**Начало области*/
-		int startX;
-		/**Конец области*/
-		int endX;
-		/**Сила действия*/
-		int power;
-		@Override
-		public String toString() {
-			return "sx: " + startX + " ex: " + endX + " p: " + power + super.toString();
-		}
-	}
+	
+	/**Ширина половины гейзера*/
+	public int width;
+	/**Середина гейзера*/
+	public int center;
+	/**Каждые сколько тиков клетку в центре будет сдувать*/
+	public int calm;
+	/**Направление гейзера*/
+	DIRECTION dir;
+	/**Фоновое изображение Гейзера*/
+	private ColorRec []fon = new ColorRec[2];
+	/** Холодный гейзер в центре */
+	private final static Color coldColor1 = new Color(0, 0, 205, 100);
+	/** Холодный гейзер скраю */
+	private final static Color coldColor2 = new Color(0, 0, 205, 0);
+	/** Горячий гейзер в центре */
+	private final static Color hotColor1 = new Color(220, 20, 60, 100);
+	/** Горячий гейзер скраю */
+	private final static Color hotColor2 = new Color(220, 20, 60, 0);
+	
 	/**
 	 * Создаёт гейзер
-	 * @param startX в рамках мира, с какой клетки начинается гейзер
-	 * @param endX в рамках мир, где он заканчивается
+	 * @param center Позиция центра гейзера
+	 * @param width ширина гейзера
 	 * @param w ширина экрана в пикселях
 	 * @param h высота экрана в пикселях
 	 * @param dir направление движения воды в гейзере - вверх или вниз
-	 * @param gradPower скорость возрастания скорости гейзера. Если будет равен 1, то гейзер выйдет на максимальную силу сразу
 	 * @param calm показывает силу работы гейзера. Каждые сколько тиков клетку в центре будет сдувать
-	 * 
-	 *  сила гейзера. Чем выше число, тем сильнее работает
 	 */
-	public Geyser(int startX, int endX,int w,int h,DIRECTION dir, int gradPower, int calm) {
-		center = startX + (endX - startX ) / 2;
+	public Geyser(int center, int width,int w,int h, DIRECTION dir, int calm) {
+		this.center = center;
 		if(dir != DIRECTION.DOWN)
 			dir = DIRECTION.UP;
-		this.startX=startX;
-		this.endX=endX;
+		this.width=width / 2;
 		this.dir=dir;
-		this.gradPower = Math.max(1, gradPower);
 		this.calm = Math.max(1, calm);
 		updateScreen(w,h);
 	}
-	/**На сколько секций делится каждый гейзер, градиент скоростей*/
-	private static int GRADIENT = 4;
-	
-	/**Начало гейзера*/
-	public int startX;
-	/**Конец гейзера*/
-	public int endX;
-	/**Середина гейзера*/
-	public int center;
-	/**Сила гейзера*/
-	public int gradPower;
-	/**Каждые сколько тиков клетку в центре будет сдувать*/
-	public int calm;
-	/**Нижняя граница, где поток имеет боковую составляющую*/
-	private int downWall;
-	/**Верхняя граница, где поток имеет боковую составляющую*/
-	private int upWall;
-	/**Направление гейзера*/
-	DIRECTION dir;
-	/**Секции, из которых состоит гейзер*/
-	Section[] cr = new Section[GRADIENT*2];
 	
 	/**
 	 * Пересчитать графическое отображение гейзера при изменении размеров экрана
@@ -93,67 +64,70 @@ public class Geyser {
 	 * @param h - высота экрана
 	 */
 	public void updateScreen(int w,int h) {
-		int lenghtSection = (int) Math.round((endX - startX)/(GRADIENT*2.0));
-		int startL = startX;
-		int endR = endX;
-		for(int i = 0 ; i < GRADIENT ; i++) {
-			int startXPxL = Point.getRx(startL);
-			int lenghtPx = Point.getRr(lenghtSection);
-			Color color;
-			if(dir == DIRECTION.DOWN)
-				color = new Color(0, 0, 205, 10 + 6*i/(GRADIENT-1));
-			else
-				color = new Color(220, 20, 60, 13 + 12*i/(GRADIENT-1));
-			cr[i] 				   = new Section(startXPxL, Configurations.border.height, lenghtPx, h-Configurations.border.height*2, color,startL,lenghtSection, 1 + gradPower * i / (GRADIENT-1));
-			if(i == GRADIENT - 1) {
-				lenghtSection = endR - (startL+lenghtSection);
-				lenghtPx = Point.getRr(lenghtSection);
-			}
-			int startXPxR = Point.getRx(endR - lenghtSection);
-			cr[GRADIENT*2 - i - 1] = new Section(startXPxR, Configurations.border.height, lenghtPx, h-Configurations.border.height*2, color,endR-lenghtSection,lenghtSection, 1 + gradPower * i / (GRADIENT-1));
-			startL += lenghtSection;
-			endR -= lenghtSection;
-		}
-		upWall = (endX - startX)/2;
-		downWall = Configurations.MAP_CELLS.height - upWall;
+		int xl[] = new int[4];
+		int yl[] = new int[4];
+		int xr[] = new int[4];
+		int yr[] = new int[4];
+		
+		xl[1] = xl[0] = Point.getRx(center - width);
+		xl[2] = xl[3] = xr[1] = xr[0] = Point.getRx(center);
+		xr[2] = xr[3] = Point.getRx(center + width);
+		
+		yl[0] = yl[3] = yr[0] = yr[3] = Point.getRy(0);
+		yl[1] = yl[2] = yr[1] = yr[2] = Point.getRy(Configurations.MAP_CELLS.height);
+
+		GradientPaint gp;
+		if(dir == DIRECTION.DOWN)
+			gp = new GradientPaint(xl[0], yl[0], coldColor2, xl[3], yl[0], coldColor1);
+		else
+			gp = new GradientPaint(xl[0], yl[0], hotColor2, xl[3], yl[0], hotColor1);
+		fon[0] = new ColorRec(xl,yl,gp);
+		
+		if(dir == DIRECTION.DOWN)
+			gp = new GradientPaint(xr[0], yr[0], coldColor1, xr[3], yr[0], coldColor2);
+		else
+			gp = new GradientPaint(xr[0], yr[0], hotColor1, xr[3], yr[0], hotColor2);
+		fon[1] = new ColorRec(xr,yr,gp);
+		
 	}
 	/**Нарисовать гейзер на экране*/
 	public void paint(Graphics g) {
-		for (ColorRec colorRec : cr)
+		for (ColorRec colorRec : fon)
 			colorRec.paint(g);
 	}
 	
 	/**Клетка проходящая через гейзер. Или не проходящая, как получится*/
 	public void action(CellObject cell) {
-		if(!(startX <= cell.getPos().getX() && cell.getPos().getX() <= endX))
+		var distX = Point.subtractionX(center, cell.getPos().getX());
+		if(Math.abs(distX) > width)
 			return;
-		for (Section se : cr) {
-			if(se.startX <= cell.getPos().getX() && cell.getPos().getX() <= se.endX) {
-				if(cell.getAge() % (calm + (gradPower - se.power + 2)) == 0) { // +2 так как se.power меняется от 1 до power + 1, а на ноль делить нельзя
-					cell.moveD(dir); // Некая сила
-					if (cell.getPos().getY() < upWall) { // Сдувание клетки в верхней части гейзера (засасывание/выталкивание)
-						boolean right = cell.getPos().getX() - center > 0;
-						if (right && dir == DIRECTION.UP || !right  && dir == DIRECTION.DOWN)
-							cell.moveD(DIRECTION.RIGHT);
-						else if(right && dir == DIRECTION.DOWN || !right  && dir == DIRECTION.UP)
-							cell.moveD(DIRECTION.LEFT);
-						else if(Math.random() < 0.5) 
-							cell.moveD(DIRECTION.RIGHT);
-						else
-							cell.moveD(DIRECTION.LEFT);
-					}  else if (cell.getPos().getY() > downWall) { // Сдувание клетки в нижней части гейзера (выталкивание/засасывание)
-						boolean right = cell.getPos().getX() - center > 0;
-						if (right && dir == DIRECTION.UP || !right  && dir == DIRECTION.DOWN)
-							cell.moveD(DIRECTION.LEFT);
-						else if(right && dir == DIRECTION.DOWN || !right  && dir == DIRECTION.UP)
-							cell.moveD(DIRECTION.RIGHT);
-						else if(Math.random() < 0.5) 
-							cell.moveD(DIRECTION.RIGHT);
-						else
-							cell.moveD(DIRECTION.LEFT);
-					} 
-				}
-				return;
+		//Сила затягивания клетки вниз. Линейный закон
+		var F = calm + Math.abs(calm * distX / width);
+		if(cell.getAge() % F == 0)
+			cell.moveD(dir); // Поехали по направлению!
+		//Сила затягивания клетки к центру. Линейный закон
+		var distY = ((double)cell.getPos().getY()) / Configurations.MAP_CELLS.height;
+		F = (int) (calm + Configurations.MAP_CELLS.height * (Math.abs((distY - 0.5) )));
+		if(cell.getAge() % F == 0) { //Затягиваемся или выталкиваемся
+			boolean right = distX > 0;
+			if(distY < 0.5) {// Сдувание клетки в верхней части гейзера (засасывание/выталкивание)
+				if (right && dir == DIRECTION.UP || !right  && dir == DIRECTION.DOWN)
+					cell.moveD(DIRECTION.RIGHT);
+				else if(right && dir == DIRECTION.DOWN || !right  && dir == DIRECTION.UP)
+					cell.moveD(DIRECTION.LEFT);
+				else if(Math.random() < 0.5) 
+					cell.moveD(DIRECTION.RIGHT);
+				else
+					cell.moveD(DIRECTION.LEFT);
+			} else {
+				if (right && dir == DIRECTION.UP || !right  && dir == DIRECTION.DOWN)
+					cell.moveD(DIRECTION.LEFT);
+				else if(right && dir == DIRECTION.DOWN || !right  && dir == DIRECTION.UP)
+					cell.moveD(DIRECTION.RIGHT);
+				else if(Math.random() < 0.5) 
+					cell.moveD(DIRECTION.RIGHT);
+				else
+					cell.moveD(DIRECTION.LEFT);
 			}
 		}
 	}
