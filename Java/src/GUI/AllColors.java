166731ade7157a965407ec1011e6f381ced09fed
Ilia Pushkin <rjhjkm20@mail.ru>
2023-09-05 17:18:29 +0300

    Сбор всех цветов
    Я планирую продумать цветовую модель мира, так что уже начал собирать цвета в одном месте!

---
+++ Java/src/GUI/AllColors.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package GUI;
+
+import Utils.Utils;
+import java.awt.Color;
+
+/**
+ * Тут перечисленны все цвета, которые отображаются на экране
+ * @author Kerravitarr
+ */
+public class AllColors {
+	/**Цвет неба, открытого воздуха*/
+	public static final Color SKY =  new Color(224, 255, 255, 255);
+	/**Цвет водички*/
+	public static final Color WATER = Utils.getHSBColor(240d / 360, 0.7, 1, 1);
+	/**Цвет земли, грязи, низа*/
+	public static final Color DRY = new Color(139, 69, 19, 255);
+	/**Цвет солнечного света*/
+	public static final Color SUN = Utils.getHSBColor(180d / 360, 1, 1, 1);
+	/**Цвет солнцечного света при минимуме*/
+	public static final Color SUN_DARK = toDark(AllColors.SUN,0);
+	/**Цвет минералов*/
+	public static final Color MINERALS = Utils.getHSBColor(300d / 360, 1, 1, 1);
+	/**Цвет минералов при минимуме*/
+	public static final Color MINERALS_DARK = toDark(AllColors.MINERALS,0);
+	
+	/**Преобразует цвет в его более прозрачный вариант
+	 * @param from какой цвет
+	 * @param alfa какая у него теперь будет альфа
+	 * @return цвет с установленной альфой
+	 */
+	public static Color toDark(Color from, int alfa){
+		return new Color(from.getRed(), from.getGreen(), from.getBlue(), alfa);
+	}
+}
