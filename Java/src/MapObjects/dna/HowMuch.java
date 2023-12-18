88645413940dcf460fc41c5941becc2fd9acba6e
Ilia Pushkin <rjhjkm20@mail.ru>
2023-12-18 16:57:04 +0300

    Обновление команды "Сколько?"
    С новыми занниями я могу делать новые вещи! :)

--- Java/src/MapObjects/dna/HowMuch.java
+++ Java/src/MapObjects/dna/HowMuch.java
@@ -10,18 +10,14 @@ import Calculations.Point;
  *
  */
 public class HowMuch extends CommandExplore {
-	public interface GetIParam {
-		public int getParam(AliveCell cell);
-	}
-	public interface GetDParam {
-		public double getParam(AliveCell cell);
+	public interface GetParam {
+		public Number getParam(AliveCell cell);
 	}
 	
 	/**Мксимальное значение функции, если такое есть*/
-	private final Integer MAX_VALUE;
+	private final Double MAX_VALUE;
 	/**Функция, которая есть параметр*/
-	private final GetIParam PARAM_I;
-	private final GetDParam PARAM_D;
+	private final GetParam PARAM;
 	/**Функция, которая есть параметр*/
 	private final boolean isTarget;
 	
@@ -31,8 +27,7 @@ public class HowMuch extends CommandExplore {
 	 * @param isT Функция будет рассматривать только себя или свою цель
 	 * @param iface функция, возвращающая значение параметра
 	 */
-	public HowMuch(String name, boolean isT, GetIParam iface) {this(name,isT, iface,null,null);};
-	public HowMuch(String name, GetDParam iface, boolean isT) {this(name,isT, null,iface,null);};
+	public HowMuch(String name, boolean isT, GetParam iface) {this(name,isT, iface,null);};
 
 	/**
 	 * Проверить некоторый параметр с ограничением на максимальное значение
@@ -41,14 +36,10 @@ public class HowMuch extends CommandExplore {
 	 * @param iface функция, возвращающая значение параметра
 	 * @param max_val максимальное значение, которое может принимать параметр
 	 */
-	public HowMuch(String name, boolean isT, GetIParam iface, Integer max_val){this(name,isT, iface,null,max_val);};
-	public HowMuch(String name, GetDParam iface, boolean isT, Integer max_val){this(name,isT, null,iface,max_val);};
-	
-	private HowMuch(String name, boolean isT, GetIParam iface, GetDParam dface, Integer max_val) {
+	public HowMuch(String name, boolean isT, GetParam iface, Number max_val) {
 		super(1, isT ? 3 : 2, isT ? name + "T" : name);
-		MAX_VALUE = max_val;
-		PARAM_I = iface;
-		PARAM_D = dface;
+		MAX_VALUE = max_val == null ? null : max_val.doubleValue();
+		PARAM = iface;
 		isTarget = isT;
 	};
 
@@ -59,18 +50,12 @@ public class HowMuch extends CommandExplore {
 			final var see = cell.see(cell.direction);
 			if (see == OBJECT.FRIEND || see == OBJECT.ENEMY) {
 				Point point = nextPoint(cell,cell.direction);
-				if(PARAM_I == null)
-					return PARAM_D.getParam((AliveCell) Configurations.world.get(point)) >= param ? 0 : 1;
-				else
-					return PARAM_I.getParam((AliveCell) Configurations.world.get(point)) >= param ? 0 : 1;
+				return PARAM.getParam((AliveCell) Configurations.world.get(point)).doubleValue() >= param ? 0 : 1;
 			} else {
 				return 2;
 			}
 		} else {
-			if(PARAM_I == null)
-				return PARAM_D.getParam(cell) >= param ? 0 : 1;
-			else
-				return PARAM_I.getParam(cell) >= param ? 0 : 1;
+			return PARAM.getParam(cell).doubleValue() >= param ? 0 : 1;
 		}
 	}
 	
