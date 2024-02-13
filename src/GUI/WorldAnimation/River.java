e2b4280c52881186d8ead0b8ecb0c7aa46a56252
Ilia Pushkin <rjhjkm20@mail.ru>
2024-02-13 17:26:17 +0300

    Шестигональное поле.
    Пока не всё гладко, но в целом нормально. Будет у меня два берега в виде шестиугольников. Добьюсь их адекватного отображения и займусь речушками!

--- src/GUI/WorldAnimation/River.java
+++ src/GUI/WorldAnimation/River.java
@@ -10,12 +10,11 @@ import GUI.WorldView;
 import Utils.ColorRec;
 import java.awt.Color;
 import java.awt.Graphics2D;
-import java.awt.geom.CubicCurve2D;
 import java.awt.geom.Path2D;
-import java.awt.geom.Point2D;
-import java.awt.geom.QuadCurve2D;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.function.Consumer;
 
 /**
  * Анимация для реки.
@@ -29,6 +28,133 @@ import java.util.List;
  * @author Kerravitarr
  */
 public class River extends DefaultAnimation{
+	/**Точка на берегу. Кубические координаты
+	 *
+	 * Рассмотрим кубическую систему координат
+	 *  
+	 *	-2,-2 -1,-2  0,-2  1,-2   2,-2  3,-2
+	 *	  -2,-1 -1,-1  0,-1  1,-1   2,-1  3,-1 
+	 *	-3,0  -2,0  -1,0   q,r   1,0   2,0   3,0
+	 *	  -3,1  -2,1  -1,1   0,1   1,1   2,1   3,1
+	 *  -4,2  -3,2  -2,2  -1,2   0,2   1,2   2,2
+	 *    -4,3  -3,3  -2,3  -1,3   0,3   1,3   2,2
+	 * 
+	 * Так что q - изменяется по диагонали. А r - строка, просто строка
+	 */
+	private static class HexPoint{
+
+		/**Направление*/
+		private static enum Direction{
+					 UP_RIGHT(-1,0),UP_LEFT(-1,1),
+					RIGHT(0,-1), CENTER(0,0),LEFT(0,1),
+					DOWN_RIGHT(1,-1),DOWN_LEFT(1,0)
+			;
+			/**Все направления*/
+			public final static Direction[] values = Direction.values();
+			/**Все направления, кроме центрального*/
+			public final static Direction[] sides = Arrays.stream(values).filter(v -> v != CENTER).toArray(Direction[]::new);
+			/**Смещение строки*/
+			private final int dr;
+			/**Смещение столбца, если строка чётная и если строка нечётная*/
+			public final int dq;
+			private Direction(int dr, int dq){this.dr = dr;this.dq = dq;}
+		}
+		
+		/**Диагональ - oblique*/
+		private int que;
+		private int row;
+		public HexPoint(){this(0,0);}
+		public HexPoint(int que, int row){this.que = que;this.row = row;}
+		/**Создаёт точку
+		 * @param d направление, в котором эта точка находится, относительно начала координат
+		 * @param radius удаление от начала координат
+		 */
+		public HexPoint(Direction d, int radius){this(d.dq*radius, d.dr*radius);}
+		
+		
+		/**Складывает две точки. Делает так, будто эта точка теперь находится не относитсельно начала координат, а относительно точки a
+		 * @param a
+		 * @return эта точка с новыми координатами
+		 */
+		public HexPoint add(HexPoint a){
+			return add(this, a);
+		}
+		public HexPoint next(Direction d){que += d.dq; row += d.dr; return this;}
+		/**Складывает две точки. Делает так, будто точка a теперь находится не относитсельно начала координат, а относительно точки b
+		 * @param a
+		 * @param b
+		 * @return точка a с новыми координатами
+		 */
+		public static HexPoint add(HexPoint a, HexPoint b){
+			a.que += b.que;
+			a.row += b.row;
+			return a;
+		}
+		
+		@Override
+		public String toString(){return "r=" + row + ";q=" + que;}
+	} 
+	/**Одна клетка берега*/
+	private static class Cell extends java.awt.geom.Path2D.Double {
+		/**Сколлько сторон будет у полигона клеток*/
+		public static final int SIDES = 6;
+		/**Центральная точка*/
+		private java.awt.geom.Point2D.Double center = new java.awt.geom.Point2D.Double(0, 0);
+		/**Радиус клетки, в пикселях*/
+		private double radius = 1;
+		
+	
+		/**Местоположение клетки*/
+		public final HexPoint point;
+		
+		/**Ил на клетке. То, что лежит на её дне*/
+		private double silt = 10;
+		/**Вода на клетке. Естественно над илом!*/
+		private double water = 0;
+		
+		public Cell(HexPoint p){
+			point = p;
+		}
+		/**Сохраняет размеры клетки, перерисовывая её заодно
+		 * @param woffset смещение относительно 0х по ширине экрана
+		 * @param scale масштаб, размер одной клетки, пк/клетку
+		 */
+		public void setScale(int woffset, double scale){
+			radius = scale * 0.9;
+			final var x = scale * (Math.sqrt(3) * point.que  +  Math.sqrt(3)/2 * point.row);
+			final var y = scale * (                         3./2 * point.row);
+			center = new java.awt.geom.Point2D.Double(x+woffset,y);
+			updatePoints();
+		}
+
+		protected void updatePoints() {
+			reset();
+			for (int p = 0; p < SIDES; p++) {
+				final var angle = (((double)p)/SIDES) * Math.PI * 2 + Math.PI / 2;
+				final var x = (center.x + Math.cos(angle) * radius);
+				final var y = (center.y + Math.sin(angle) * radius);
+				if(p == 0){
+					moveTo(x, y);
+				} else {
+					lineTo(x, y);
+				}
+			}
+		}
+		public void draw(Graphics2D g, int lineThickness, int colorValue) {
+			// Store before changing.
+			final var tmpS = g.getStroke();
+			final var tmpC = g.getColor();
+
+			g.setColor(new java.awt.Color(colorValue));
+			g.setStroke(new java.awt.BasicStroke(lineThickness, java.awt.BasicStroke.CAP_SQUARE, java.awt.BasicStroke.JOIN_MITER));
+
+			g.fill(this);
+
+			// Set values to previous when done.
+			g.setColor(tmpC);
+			g.setStroke(tmpS);
+		}
+	}
 	/**Волна. Её один элемент*/
 	private static class Wave{
 		/**Размер волны. Расстояние между точками волны*/
@@ -102,6 +228,14 @@ public class River extends DefaultAnimation{
 		private double speed1 = (minV + maxV) / 2;
 		private double speed2 = speed1;
 		
+		/**Карта самого поля
+		 * Представляет собой высокосортную жесть:
+		 * Каждый ряд карты (первое число) - row.
+		 * А каждый столбец (второе числоа) - q.
+		 * А жесть в том, что в квадратной гесанальной сетке часть клеток... Пустота! null
+		 */
+		private Cell[][] map = new Cell[0][0];
+		
 		/**Добавляет волны */
 		private void addWave(int newSize){
 			realCW = newSize + 3; //Дополнительные нужны, чтобы колебания выглядели натурально, хотя часть волн не могут колебаться сами по себе
@@ -112,7 +246,7 @@ public class River extends DefaultAnimation{
 			countWave = newSize;
 		}
 		/**Установить количество волн на берегу*/
-		public void setSize(int newSize) {
+		public void setCountWaves(int newSize) {
 			if (countWave > newSize) {
 				countWave = newSize;
 				realCW = newSize + 3;
@@ -120,12 +254,11 @@ public class River extends DefaultAnimation{
 				addWave(newSize);
 			}
 		}
-		
 		/**Обновляет волны
 		 * @param border граница игрового поля. Волны должны бегать вокруг него
 		 * @param isLeft волны слева от границы бегают?
 		 */
-		private void updateWaves(int border, boolean isLeft){
+		public void updateWaves(int border, boolean isLeft){
 			final var first = waves.get(0);
 			final var last = waves.get(realCW - 1);
 			{
@@ -174,6 +307,69 @@ public class River extends DefaultAnimation{
 				}
 			}
 		}
+		/**Обновляет дополнительные реки
+		 * @param height высота игрового поля в клетках
+		 * @param isLeft волны слева от границы бегают?
+		 */
+		public void regenerateRiver(int height, boolean isLeft) {
+			map = new Cell[height][height + (height / 2 + height % 2)];
+			for (int r = 0; r < height; r++) {
+				for (int c = 0; c < height; c++) {
+					final var q = c - (r + (r&1)) / 2;
+					map[r][c] = new Cell(new HexPoint(q, r));
+				}
+			}
+			final var Rs = height / 2; //Радиус реки изначально.
+			//А теперь прокопаем реку!
+			//Находим точку, удалённую от центра копания - то есть от верхнего угла, в одну из сторон
+			HexPoint point;
+			if(isLeft)
+				point = (new HexPoint(HexPoint.Direction.sides[0], Rs)).add(new HexPoint(height,0));
+			else
+				point = (new HexPoint(HexPoint.Direction.sides[0], Rs)).add(new HexPoint());
+			//И копаем по кругу
+			for (final var d : HexPoint.Direction.sides) {
+				for (int j = 0; j < Rs; j++) {
+					if(isValid(point)){
+						final var cell = get(point);
+						cell.silt /= 2; //В русле глубина в половину ниже
+					}
+					point = point.next(d);
+				}
+			}
+			//А теперь с другой стороны копаем
+			if(isLeft)
+				point = (new HexPoint(HexPoint.Direction.sides[0], Rs)).add(new HexPoint(height/2,height));
+			else
+				point = (new HexPoint(HexPoint.Direction.sides[0], Rs)).add(new HexPoint(-height/2, height));
+			//И копаем по кругу
+			for (final var d : HexPoint.Direction.sides) {
+				for (int j = 0; j < Rs; j++) {
+					if(isValid(point)){
+						final var cell = get(point);
+						cell.silt /= 2; //В русле глубина в половину ниже
+					}
+					point = point.next(d);
+				}
+			}
+		}
+		
+		public void forEach(Consumer<Cell> action){
+			final var height = map.length;
+			for (int r = 0; r < height; r++) {
+				for (int c = 0; c < height; c++) {
+					final var q = c - (r + (r&1)) / 2;
+					action.accept(map[r][c]);
+				}
+			}
+		}
+		/**Сохранить размеры поля рядом с рекой
+		 * @param woffset смещение относительно нуля
+		 * @param scale размер клетки, пк/клетку
+		 */
+		private void setSize(int woffset, double scale) {
+			forEach(c -> c.setScale(woffset, scale));
+		}
 		/**Строит дугу
 		 * @param where к какому пути добавить дугу
 		 * @param x1 координаты предыдущей точки
@@ -208,6 +404,20 @@ public class River extends DefaultAnimation{
 			where.curveTo(cx2a, cy2a, cx2b, cy2b, x3, y3);
 			//where.lineTo(x3, y3);
 		}
+		/**Проверяет точку на принадлежность к полю*/
+		private boolean isValid(HexPoint p){
+			final var rq = p.que + p.row / 2;
+			return p.row >= 0 && p.row < map.length && rq >= 0 && rq < map[0].length;
+		}
+		/**Возвращает ячейку поля
+		 * @param p
+		 * @return 
+		 */
+		private Cell get(HexPoint p){
+			assert isValid(p) : "Точка оказалась за границей! Точка " + p + " при поле " + map.length + "х"+ map.length;
+			return map[p.row][p.que + p.row / 2];
+		}
+
 	}
 	/**Все статические переменные*/
 	private static class Static{
@@ -219,16 +429,32 @@ public class River extends DefaultAnimation{
 		public final RiverBank right = new RiverBank();
 		
 		/**Установить количество волн на обоих берегах*/
-		public void setSize(int newSize){
-			left.setSize(newSize);
-			right.setSize(newSize);
+		public void setCountWaves(int newSize){
+			left.setCountWaves(newSize);
+			right.setCountWaves(newSize);
 			countWave = newSize;
 		}
 		
 		/**Обновляет волны*/
-		private void updateWaves(int leftBorder, int rightBorder){
+		public void updateWaves(int leftBorder, int rightBorder){
 			left.updateWaves(leftBorder, true);
 			right.updateWaves(rightBorder, false);
+		}
+		/**Обновляет дополнительные реки
+		 * @param height высота игрового поля в клетках
+		 */
+		public void regenerateRiver(int height) {
+			left.regenerateRiver(height, true);
+			right.regenerateRiver(height, false);
+		}
+		/**Сохраняет размеры для берегов
+		 * @param leftWOff Смещение игрового поля для левого берега
+		 * @param rightWOff Смещение игрового поля для правого берега
+		 * @param scale масштаб, пк/клетку
+		 */
+		private void setSize(int leftWOff,int rightWOff, double scale) {
+			left.setSize(leftWOff,scale);
+			right.setSize(rightWOff, scale);
 		}
 	}
 	
@@ -247,6 +473,8 @@ public class River extends DefaultAnimation{
 	private final int leftBorder;
 	/**Правая граница игрового поля*/
 	private final int rightBorder;
+	/**Масштаб или размер одной клетки шестиугольника, пк/клетку*/
+	private final double scale;
 	
 	public River(WorldView.Transforms transform, int w, int h){
 		//Левый песочек
@@ -271,15 +499,20 @@ public class River extends DefaultAnimation{
 		countWave = (int) Math.ceil(h / Wave.WIDTH);
 		leftBorder = transform.toScrinX(0);
 		rightBorder = transform.toScrinX(Configurations.getWidth()-1);
+		final var ch = 10;
+		scale = ((double)h) / ch;
 		
 		if(state == null) state = new Static();
+		if(state.left.map.length != ch)
+			state.regenerateRiver(ch);
+		state.setSize( 0, rightBorder, scale);
 	}
 		
 	@Override
 	protected void nextFrame(){
 		if(countWave <= 0) return;
 		if(state.countWave != countWave)
-			state.setSize(countWave);
+			state.setCountWaves(countWave);
 		state.updateWaves(leftBorder, rightBorder);
 	}
 
@@ -296,6 +529,9 @@ public class River extends DefaultAnimation{
 		g.setColor(AllColors.WATER_RIVER);
 		g.fill(state.left.figure);
 		g.fill(state.right.figure);
+		
+		state.left.forEach(c -> c.draw(g, 0, 0x008844));
+		//state.right.forEach(c -> c.draw(g, 0, 0x008844));
 		
 	}
 	
